2f97b89015021f8dae0ca8324869b36309de57bc
Matt Umland <mattumland@gmail.com>
2021-03-24 08:30:08 -0400

    Add movie data file to repo and import into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import movieData from './movieData';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      movieData: movieData.movies
+    }
   }
 
   render() {
