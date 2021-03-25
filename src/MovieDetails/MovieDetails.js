26d525d9b301c8d3415aff1f1f44accd90e6d4dd
Anneke Miers <annekemiers.am@gmail.com>
2021-03-24 21:21:20 -0600

    Add fake movie details

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import './MovieDetails.css';
 
+const MovieDetails = ({})
+
+
+//Accepts one instance of movieDetail as a prop, accessed via the id 
 
 export default MovieDetails;
