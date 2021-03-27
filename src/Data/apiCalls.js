ae6a4623abccc0ea53b5f82f9e2628fdc17db913
Anneke Miers <annekemiers.am@gmail.com>
2021-03-27 17:56:36 -0600

    Create base api calls

---
+++ src/Data/apiCalls.js
@@ -0,0 +1,23 @@
+const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2'
+
+export const fetchAllMovies = () => {
+  return fetch(`${baseURL}/movies`)
+    .then(response => {
+      return response.json()
+    })
+}
+
+export const fetchSingleMovie = (id) => {
+  // return fetch(`${baseURL}/movies/:movie_id`)
+  return fetch(`${baseURL}/movies/${id}`)
+    .then(response => {
+      return response.json()
+    })
+}
+
+export const fetchVideo = (id) => {
+  return fetch(`${baseURL}/movies/${id}/videos`)
+    .then(response => {
+      return response.json()
+    })
+}
