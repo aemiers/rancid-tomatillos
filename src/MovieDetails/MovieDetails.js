4baeca419394bc82dd237b79301f78dd1423ac55
Matt Umland <mattumland@gmail.com>
2021-03-29 17:32:59 -0400

    Add MovieDetails prop types

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './MovieDetails.scss';
 
 const MovieDetails = ({ movie, icon }) => {
@@ -41,3 +42,7 @@ const MovieDetails = ({ movie, icon }) => {
 
 export default MovieDetails;
 
+MovieDetails.propTypes = {
+  movie: PropTypes.object,
+  icon: PropTypes.string
+};
