8f9d018c82f4d29005783b90cec447101cba12da
Anneke Miers <annekemiers.am@gmail.com>
2021-04-01 21:46:35 -0600

    Add trailer data for cypress

--- cypress/integration/examples/rancid_tomatillos_spec.js
+++ cypress/integration/examples/rancid_tomatillos_spec.js
@@ -10,110 +10,83 @@ describe('Rancid Tomatillos Home Page', () => {
     cy
       .get('.logo').should('be.visible')
       .get('.form-container').should('be.visible')
+      .get('input').should('be.visible')
+      .get('input').should('be.empty')
+  });
+
+  it('Should show all movie mini posters', () => {
+    cy
+      .get('.movie-card')
+      .should(($movieCard) => {
+        expect($movieCard).to.have.length(3)
+        expect($movieCard, 'first').to.contain('Money Plane')
+        expect($movieCard, 'second').to.contain('Mulan')
+        expect($movieCard, 'third').to.contain('Rogue')
+      })
   });
 
   it('Should be able to search movies', () => {
     cy
       .get('input').type('mU')
-      .should(($movieGrid) => {
-        expect($movieGrid).to.have.length(1)
+      .get('.movie-card')
+      .should(($movieCard) => {
+        expect($movieCard).to.have.length(1)
+        expect($movieCard).to.contain('Mulan')
       })
   });
 
-  it('Should show all movie mini posters', () => {
+  // it('Should display a message if no movies match search criteria', () => {
+  //   cy
+  //     .get('input').type('ava')
+  //     .get('.movieGrid')
+  //     .should(($movieGrid) => {
+  //       expect($movieGrid).to.contain('Insert proper error message here')
+  //     })
+  // });
+
+  // it('Should be able to click on a mini-movie poster and be redirected to a new link', () => {
+  //   cy
+  //     .on('uncaught:exception', (err, runnable) => {
+  //       return false
+  //     })
+  //     .get('.movie-card:first').click()
+  //     .get('.movie-title').contains('Test 1')
+  //     .url().should('include', '694919')
+  // });
+});
+
+describe('Movie Details Page', () => {
+  beforeEach(() => {
     cy
-      .get('.movieGrid')
-      .should(($movieGrid) => {
-        expect($movieGrid).to.have.length(3)
-      })
+      .intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919/videos', { fixture: 'trailer-data.json' })
+      .intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', { fixture: 'single-movie-data.json' })
+      .intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'mock-movie-data.json' })
+      .visit('http://localhost:3000')
+    // .get('.poster:first').click()
+  })
+
+  it('Should display the movie\'s title, rating, genre, year, and run time', () => {
+    cy
+      .url().should('include', '694919')
+    // .get('.movie-title').should.contain('Test 1')
+    // .get('.trailer').contains('Trailer')
+    // .get('.backdrop').contains('oazPqs1z78LcIOFslbKtJLGlueo')
+    // .get('.movie-words__title').contains('Fake')
+    // .get('.movie-words__details').contains('buzzwords')
+    // .get('.tomatillo').should('have.attr', 'src')
+    //         { calculatePercent(movie.average_rating)
+    // }% · { formatGenre(movie.genres)} · { formatYear(movie.release_date) } · { formatRunTime(movie.runtime) }</h2 >
+    //   <p className='overview'>{movie.overview}</p>
   });
-  //   .get('.poster').should('be.visible')
-  // .get('.rating').contains('%')
-  // .get('.hover-title').contains('Money Plane')
+
 })
 
 
-//   it('Should be able to visit the page and render the correct elements', () => {
-  //     cy.contains('Movies');
-  //   });
-  //   // it('Should display an error message if the movies cannot be loaded', () => {
-  //   //   // cy.fixture('movie_data.json').then((movies) => {
-  //   //   cy.intercept(
-  //   //     'GET',
-  //   //     'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
-  //   //     {
-  //   //       statusCode: 500,
-  //   //       body: {
-  //   //         message: `Something went wrong`
-  //   //       }
-  //   //     }
-  //   //   )
-  //   //   // })
-  //   //   cy.get('.error').should('contain', 'Something went wrong')
-  //   // })
 
 
-  //   // it('Should filter movie options with the search bar', () => {
-  //   // });
+it('Should play the trailer when clicked on', () => {
+});
 
-  //   // it('Should display a message if no movies match search criteria', () => {
-  //   // });
-
-  //   it('Should be able to click on a mini-movie poster and be redirected to a new link', () => {
-  //     cy.fixture('movie_data.json').then((movies) => {
-  //       cy.intercept({
-  //         method: 'GET',
-  //         url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
-  //       },
-  //         {
-  //           statusCode: 201,
-  //           body: {
-  //             movies: movies,
-  //           }
-  //         }
-  //       )
-  //     });
-  //     // cy.get('.movie-card:first').click()
-  //     // cy.url().should('include', '/movie/694919')
-  //   });
-
-  //   // describe('Movie Details Page', () => {
-
-  //   //   beforeEach(() => {
-  //   //     cy.visit('http://localhost:3000')
-  //   //   })
-
-  //   //   it('Should display a movie backdrop', () => {
-  //   //     cy.fixture('movie_data.json').then((movies) => {
-  //   //       cy.intercept(
-  //   //         'GET',
-  //   //         'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919',
-  //   //       )
-  //   //     })
-  //   //     // cy.get('.movie-details-section').should('be.visible')
-  //   //     cy.get('.backdrop')
-  //   //     cy.get('.rating').contains('61')
-  //   //     cy.get('.hover-title').contains('Money Plane')
-  //   //   });
-
-  //       it('Should display the movie\'s title, rating, genre, year, and run time,'() => {
-  //         cy.get('.backdrop').contains('oazPqs1z78LcIOFslbKtJLGlueo')
-  //         cy.get('.movie-words__title').contains('Fake')
-  //         cy.get('.movie-words__details').contains('buzzwords')
-  //         cy.get('.tomatillo').should('have.attr', 'src')
-  // //         { calculatePercent(movie.average_rating)
-  // // }% · { formatGenre(movie.genres)} · { formatYear(movie.release_date) } · { formatRunTime(movie.runtime) }</h2 >
-  // //   <p className='overview'>{movie.overview}</p>
-  //       });
-
-
-  //   //   //   it('Should display a picture in place of the movie', () => {
-  //   //   //   });
-
-  //   //   //   it('Should play the trailer when clicked on', () => {
-  //   // });
-
-// });
 
 // testing error message 
 // it('should show an error message when the server does not respond', () => {
@@ -122,7 +95,7 @@ describe('Rancid Tomatillos Home Page', () => {
 //     .get('h2').contains('Oops')
 // })
 
-// it('cy.go() - go back or forward in the browser\'s history', () => {
+// it('should go back or forward in the browser\'s history', () => {
 //   // https://on.cypress.io/go
 
 //   cy.location('pathname').should('include', 'navigation')
@@ -160,3 +133,19 @@ describe('Rancid Tomatillos Home Page', () => {
 //     },
 //   })
 // })
+
+// it('Should display an error message if the movies cannot be loaded', () => {
+  //   // cy.fixture('movie_data.json').then((movies) => {
+  //   cy.intercept(
+  //     'GET',
+  //     'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+  //     {
+  //       statusCode: 500,
+  //       body: {
+  //         message: `Something went wrong`
+  //       }
+  //     }
+  //   )
+  //   // })
+  //   cy.get('.error').should('contain', 'Something went wrong')
+  // })
