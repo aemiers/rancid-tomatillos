20a488cecddd9f1202ed7f7c8ab866388a08fc7c
Anneke Miers <annekemiers.am@gmail.com>
2021-04-03 09:23:54 -0600

    Move filter section

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -23,6 +23,16 @@ const MovieList = ({ movies, calc, icon }) => {
   return (
     <section className='mainList'>
       <h1>Movies</h1>
+      <section className='filter-container'>
+        <button id="action" class="filter-button" type="submit" value="action">Action</button>
+        <button id="adventure" class="filter-button" type="submit" value="adventure">Adventure</button>
+        <button id="comedy" class="filter-button" type="submit" value="comedy">Comedy</button>
+        <button id="drama" class="filter-button" type="submit" value="drama">Drama</button>
+        <button id="fantasy" class="filter-button" type="submit" value="fantasy">Fantasy</button>
+        <button id="family" class="filter-button" type="submit" value="family">Family</button>
+        <button id="horror" class="filter-button" type="submit" value="horror">Horror</button>
+        <button id="romance" class="filter-button" type="submit" value="romance">Romance</button>
+      </section>
       <section className='movieGrid'>
         {movieCards}
       </section>
